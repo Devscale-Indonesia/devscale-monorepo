9a8ca1bddd89b3a1fda56e51a43222b4aba64e0a
Indra Zulfi <me@indrazm.com>
2023-09-18 13:15:12 +0700

    chore: initialization backend nextjs

--- apps/platform/app/page.tsx
+++ apps/platform/app/page.tsx
@@ -1,5 +1,13 @@
 import { Auth } from '../components/Auth/components/Auth';
 
+async function getData() {
+  const res = await fetch('http://localhost:4201/api/hello', { cache: 'no-store' });
+  const data = await res.json();
+  return data;
+}
+
 export default async function Page() {
+  const data = await getData();
+  console.log(data);
   return <Auth />;
 }
